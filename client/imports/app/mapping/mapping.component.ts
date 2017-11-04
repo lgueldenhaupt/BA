3eb41d003a20cf46be0855c08d642dfcfc1538f4
Lukas <lgueldenhaupt@gmail.com>
2017-11-04 19:54:31 +0100

    edited mapping page to contain mappings

--- client/imports/app/mapping/mapping.component.ts
+++ client/imports/app/mapping/mapping.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from "@angular/core";
 import template from "./mapping.component.html";
 import style from "./mapping.component.scss";
+import {MappingsDataService} from "../../services/mappings-data.service";
+import {Observable} from "rxjs/Observable";
+import {Mapping} from "../../../../both/models/mapping.model";
 
 declare let $ :any;
 
@@ -10,12 +13,19 @@ declare let $ :any;
     styles: [ style ]
 })
 export class MappingComponent implements OnInit{
+    private mappings: Observable<Mapping[]>;
+    private selectedMapping: Mapping;
 
     constructor(
+        private mappingDS: MappingsDataService
     ) {
     }
 
     ngOnInit(): void {
+        this.mappings = this.mappingDS.getData().zone();
+    }
 
+    public selectMapping(mapping: Mapping) {
+        this.selectedMapping = mapping;
     }
 }
