4de05450fc8e7a131547be3dc802e8f334d226de
Lukas <lgueldenhaupt@gmail.com>
2018-01-07 16:07:27 +0100

    added ssl package and shitty version of it

--- server/imports/server-main/main.ts
+++ server/imports/server-main/main.ts
@@ -4,8 +4,11 @@ import {ProjectsCollection} from "../../../both/collections/projects.collection"
 import {ConfigSetsCollection} from "../../../both/collections/configsets.collection";
 import {MappingsCollection} from "../../../both/collections/mappings.collection";
 
+declare let SSL : any;
+
 export class Main {
     start(): void {
+        SSL('C:/Users/lguel/server.key', 'C:/Users/lguel/server.cert', 442);
         Meteor.users.allow({
             update: function() {
                 return true;
